df5138d4d428405fa96f059563877011c1098891
IRUMVAEmmanuel1 <emmanuelirumva1@gmail.com>
2023-08-04 18:51:34 +0300

    weather fetching api

--- app.js
+++ app.js
@@ -1,49 +1,38 @@
-const apiKey = 'YOUR_WEATHER_API_KEY'; // Replace with your WeatherAPI key
+const apiKey = 'YOUR_OPENWEATHERMAP_API_KEY';
 
-(async () => {
-  const getWeatherData = async (location) => {
-    const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location}`;
-    try {
-      const response = await fetch(apiUrl);
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.error('Error fetching weather data:', error);
-      return null;
-    }
+function getWeatherData(location) {
+  return fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}`)
+    .then(response => response.json());
+}
+
+function processWeatherData(data) {
+  return {
+    city: data.name,
+    description: data.weather[0].description,
+    temperature: data.main.temp,
+    humidity: data.main.humidity,
+    icon: data.weather[0].icon,
   };
+}
 
-  const extractWeatherData = (data) => {
-    if (!data) return null;
-    const location = data.location.name;
-    const temperatureC = data.current.temp_c;
-    const temperatureF = data.current.temp_f;
-    const condition = data.current.condition.text;
-    const iconUrl = data.current.condition.icon;
-    return { location, temperatureC, temperatureF, condition, iconUrl };
-  };
+document.getElementById('weatherForm').addEventListener('submit', (event) => {
+  event.preventDefault();
+  const locationInput = document.getElementById('locationInput');
+  const location = locationInput.value.trim();
 
-  const weatherForm = document.getElementById('weatherForm');
-  const weatherInfoDiv = document.getElementById('weatherInfo');
-
-  weatherForm.addEventListener('submit', async (event) => {
-    event.preventDefault();
-    const locationInput = document.getElementById('locationInput');
-    const location = locationInput.value;
-    locationInput.value = ''; // Clear the input field after submission
-
-    const data = await getWeatherData(location);
-    const weatherData = extractWeatherData(data);
-
-    if (weatherData) {
-      weatherInfoDiv.innerHTML = `
-        <h2>${weatherData.location}</h2>
-        <p>${weatherData.condition}</p>
-        <p>Temperature: ${weatherData.temperatureC}°C / ${weatherData.temperatureF}°F</p>
-        <img src="${weatherData.iconUrl}" alt="${weatherData.condition}">
-      `;
-    } else {
-      weatherInfoDiv.innerHTML = '<p>Weather data not available. Please try again.</p>';
-    }
-  });
-})();
+  if (location !== '') {
+    document.getElementById('loading').classList.remove('hidden');
+    getWeatherData(location)
+      .then(data => {
+        document.getElementById('loading').classList.add('hidden');
+        document.getElementById('weatherData').innerText = JSON.stringify(processWeatherData(data), null, 2);
+        document.getElementById('weatherInfo').classList.remove('hidden');
+      })
+      .catch(error => {
+        console.error('Error fetching weather data:', error);
+        document.getElementById('loading').classList.add('hidden');
+        document.getElementById('weatherData').innerText = 'Error fetching weather data.';
+        document.getElementById('weatherInfo').classList.remove('hidden');
+      });
+  }
+});

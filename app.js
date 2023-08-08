fb4fbb729396474875b42a80e0d86faaf0b3c54b
IRUMVAEmmanuel1 <emmanuelirumva1@gmail.com>
2023-08-08 22:44:47 +0300

    async and checkWeather function

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const apiKey = "ba6f260343e0ba0afd7fa5521964b6c1"
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
 
-async function checkWeather(){
+async function checkWeather(city){
   const response = await fetch(apiUrl + `&appid=${apiKey}`);
   var data = await response.json();
   console.log(data)

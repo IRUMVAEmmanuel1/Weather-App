e90b012041619c4e26bad829dfe26df2305641e9
IRUMVAEmmanuel1 <emmanuelirumva1@gmail.com>
2023-08-08 22:38:09 +0300

    async and checkWeather function

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const apiKey = "ba6f260343e0ba0afd7fa5521964b6c1"
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=kigali";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=rubavu";
 
 async function checkWeather(){
   const response = await fetch(apiUrl + `&appid=${apiKey}`);
   var data = await response.json();
   console.log(data)
 
-  document.querySelector(".city").innerHTML = data
+  document.querySelector(".city").innerHTML = data.name;
+  document.querySelector(".temp").innerHTML = data.main.temp;
+  document.querySelector(".humidity").innerHTML = data.main.humidity;
+  document.querySelector(".wind").innerHTML = data.wind.speed;
 }
 checkWeather();

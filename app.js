f0050cd5715695fa6b2101217ae9da5413b40072
IRUMVAEmmanuel1 <emmanuelirumva1@gmail.com>
2023-08-08 22:25:00 +0300

    async and checkWeather function

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
 const apiKey = "ba6f260343e0ba0afd7fa5521964b6c1"
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=kigali";
+
+async function checkWeather(){
+  const response = await fetch(apiUrl + `&appid=${apiKey}`);
+}
